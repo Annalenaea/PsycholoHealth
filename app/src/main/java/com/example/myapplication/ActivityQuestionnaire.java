661c255e089eb605dcf2607aa065e5c8d6b549bb
Vici <victorine.schmidt@st.ovgu.de>
2022-01-02 19:56:05 +0100

    CheckboxFunctionsActivityQuestionaire

--- app/src/main/java/com/example/myapplication/ActivityQuestionnaire.java
+++ app/src/main/java/com/example/myapplication/ActivityQuestionnaire.java
@@ -4,16 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.myapplication.databinding.ActivityQuestionnaireBinding;
+
+import java.util.ArrayList;
 
 public class ActivityQuestionnaire extends Fragment {
 
     private ActivityQuestionnaireBinding binding;
+    private CheckBox CBDomesticWork, CBSelfCare, CBParty, CBHobby;
+    private Button Finish;
+    private ArrayList<String> CBResult;
+
 
     @Override
     public View onCreateView(
@@ -22,9 +29,61 @@ public class ActivityQuestionnaire extends Fragment {
     ) {
 
         binding = ActivityQuestionnaireBinding.inflate(inflater, container, false);
-        return binding.getRoot();
 
+
+        CBDomesticWork = binding.checkboxDomesticWork;
+        CBSelfCare = binding.checkboxSelfCare;
+        CBParty= binding.checkboxParty;
+        CBHobby = binding.checkboxHobbies;
+        Finish = binding.ButtonFinish;
+        CBResult = new ArrayList<>();
+
+        CBDomesticWork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (CBDomesticWork.isChecked())
+                    CBResult.add("DomesticWork");
+                else
+                    CBResult.remove("DomesticWork");
+            }
+        });
+        CBSelfCare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (CBSelfCare.isChecked())
+                    CBResult.add("SelfCare");
+                else
+                    CBResult.remove("SelfCare");
+            }
+        });
+        CBParty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (CBParty.isChecked())
+                    CBResult.add("Party");
+                else
+                    CBResult.remove("Party");
+            }
+        });
+        CBHobby.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (CBHobby.isChecked())
+                    CBResult.add("Hobby");
+                else
+                    CBResult.remove("Hobby");
+            }
+        });
+        Finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // GO BACK TO MAIN
+            }
+        });
+
+        return binding.getRoot();
     }
+
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
